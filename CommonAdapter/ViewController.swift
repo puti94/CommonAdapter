0a5fdd4e65b007ebe012b4da02698cf2cd361e32
putixie <1059592160@qq.com>
2017-11-22 10:28:43 +0800

    Initial Commit

---
+++ CommonAdapter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommonAdapter
+//
+//  Created by puti on 2017/11/22.
+//  Copyright © 2017年 puti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
